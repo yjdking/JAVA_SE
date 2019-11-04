af3cfb2601d8e8131e2f339a2d53f3a72358f5e1
king <419562071@qq.com>
2019-11-04 22:33:53 +0800

    java8 lamda test

---
+++ src/newfeatures/java8/Ladmda.java
@@ -0,0 +1,13 @@
+package newfeatures.java8;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ladmda {
+    public List arrayList = Arrays.asList(1,2,3);
+
+    public List<String> arrayList = Arrays.asList("1","2");
+}
+
+
