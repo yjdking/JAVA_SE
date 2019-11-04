c8d41440516caac2fa887095906092d91b3fb59d
yjdking <419562071@qq.com>
2019-11-04 23:16:47 +0800

    Arrays.aslist

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import newfeatures.java7.Decimal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+        String[] s = {"aa","bb","cc"};
+        List<String> strlist = Arrays.asList(s);
 
     }
 }
---
+++ src/newfeatures/java8/Lamda.java
@@ -0,0 +1,14 @@
+package newfeatures.java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lamda {
+    public List arrayList = (List) Arrays.asList(1,2,3);
+
+    public List<String> arrayList1 = Arrays.asList("1","2");
+
+}
+
+
